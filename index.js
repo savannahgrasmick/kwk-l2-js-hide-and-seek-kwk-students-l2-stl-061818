40855171fe42f79c035bcd5cb644799ef206915c
savannahgrasmick <grasmicksavannah@gmail.com>
2018-06-21 21:25:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Code your solution here
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("div#grand-node div div div div")
+}
